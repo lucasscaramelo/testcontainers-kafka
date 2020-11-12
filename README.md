bdec394ecd1329f8aa147e50cf9cfffff7c5ffbc
Lucas Scaramelo <lucas.scaramelo@gmail.com>
2020-11-12 18:49:11 -0300

    Estrutura inicial

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # TestContainers-Kafka
+
+Kafka  tem como objetivo fornecer uma  plataforma unificada, de alta capacidade e baixa latência para tratamento de dados em tempo real.
+
+O Apache Kafka é baseado no commit log, ele permite que os usuários inscrevam-se e publiquem dados para qualquer número de sistemas ou aplicações em tempo real.
+
+Com TestContainers, criaremos uma imagem do Kafka para efetuar uma rotina determinada de testes automazidas que ao seu final de execução, matará todos os containers executados.
---
+++ src/test/java/org/apache/kafka/KafkaContainerTest.java
@@ -0,0 +1,108 @@
+package org.apache.kafka;
+
+import static java.util.Collections.singletonList;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
+
+import com.google.common.collect.ImmutableMap;
+import java.time.Duration;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.junit.Test;
+import org.rnorth.ducttape.unreliables.Unreliables;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.containers.Network;
+import org.testcontainers.utility.DockerImageName;
+
+public class KafkaContainerTest {
+
+    private static final DockerImageName KAFKA_TEST_IMAGE = DockerImageName.parse("confluentinc/cp-kafka");
+    private static final DockerImageName ZOOKEEPER_TEST_IMAGE = DockerImageName.parse("confluentinc/cp-zookeeper");
+
+    @Test
+    public void testUsage() throws Exception {
+        try (KafkaContainer kafka = new KafkaContainer(KAFKA_TEST_IMAGE)) {
+            kafka.start();
+        }
+    }
+
+    @Test
+    public void testExternalZookeeperWithExternalNetwork() throws Exception {
+        try (
+                Network network = Network.newNetwork();
+
+                KafkaContainer kafka = new KafkaContainer(KAFKA_TEST_IMAGE)
+                        .withNetwork(network)
+                        .withExternalZookeeper("zookeeper:2181");
+
+                GenericContainer<?> zookeeper = new GenericContainer<>(ZOOKEEPER_TEST_IMAGE)
+                        .withNetwork(network)
+                        .withNetworkAliases("zookeeper")
+                        .withEnv("ZOOKEEPER_CLIENT_PORT", "2181");
+
+                GenericContainer<?> application = new GenericContainer<>(DockerImageName.parse("alpine"))
+                        .withNetwork(network)
+                        .withNetworkAliases("dummy")
+                        .withCommand("sleep 10000")
+        ) {
+            zookeeper.start();
+            kafka.start();
+            application.start();
+
+            testKafkaFunctionality(kafka.getBootstrapServers());
+        }
+    }
+
+    protected void testKafkaFunctionality(String bootstrapServers) throws Exception {
+        try (
+                KafkaProducer<String, String> producer = new KafkaProducer<>(
+                        ImmutableMap.of(
+                                ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers,
+                                ProducerConfig.CLIENT_ID_CONFIG, UUID.randomUUID().toString()
+                        ),
+                        new StringSerializer(),
+                        new StringSerializer()
+                );
+
+                KafkaConsumer<String, String> consumer = new KafkaConsumer<>(
+                        ImmutableMap.of(
+                                ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers,
+                                ConsumerConfig.GROUP_ID_CONFIG, "tc-" + UUID.randomUUID(),
+                                ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest"
+                        ),
+                        new StringDeserializer(),
+                        new StringDeserializer()
+                );
+        ) {
+            String topicName = "messages-" + UUID.randomUUID();
+            consumer.subscribe(singletonList(topicName));
+
+            producer.send(new ProducerRecord<>(topicName, "testcontainers", "rulezzz")).get();
+
+            Unreliables.retryUntilTrue(10, TimeUnit.SECONDS, () -> {
+                ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+
+                if (records.isEmpty()) {
+                    return false;
+                }
+
+                assertThat(records)
+                        .hasSize(1)
+                        .extracting(ConsumerRecord::topic, ConsumerRecord::key, ConsumerRecord::value)
+                        .containsExactly(tuple(topicName, "testcontainers", "rulezzz"));
+                return true;
+            });
+            consumer.unsubscribe();
+        }
+    }
+}
